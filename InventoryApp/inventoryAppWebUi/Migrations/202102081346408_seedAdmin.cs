de607bb7d1baffe46a7dfc761b435e8e53c9f09b
Nwankwu Tochukwu <t.nwankwu@genesystechhub.com>
2021-02-08 15:53:58 +0100

    authentication work

---
+++ InventoryApp/inventoryAppWebUi/Migrations/202102081346408_seedAdmin.cs
@@ -0,0 +1,16 @@
+namespace inventoryAppWebUi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class seedAdmin : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
