ecd73d0b2707c9b27e00c4518ba3b0037eced849
Ade-Joshe01 <j.adegoke@genesystechhub.com>
2021-02-10 05:30:55 +0100

    BackEnd Merge with Authentication UI

--- InventoryApp/inventoryAppWebUi/Models/IdentityModels.cs
+++ InventoryApp/inventoryAppWebUi/Models/IdentityModels.cs
@@ -1,11 +1,10 @@
 ﻿using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using inventoryAppDomain.Entities;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace inventoryAppDomain.IdentityEntities
+namespace inventoryAppWebUi.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
@@ -21,12 +20,6 @@ namespace inventoryAppDomain.IdentityEntities
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Brand> Brands { get; set; }
-        public DbSet<Supplier> Suppliers { get; set; }
-        public DbSet<Drug> Drugs { get; set; }
-        //public DbSet<Role> MyProperty { get; set; }
-
-
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
