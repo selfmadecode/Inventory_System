64cd5e72171eb1b4cf012d2b9e8fb84bda2b3a75
Nwankwu Tochukwu <t.nwankwu@genesystechhub.com>
2021-03-04 12:12:22 +0100

    cron jobs logic update

---
+++ InventoryApp/inventoryAppWebUi/Infrastructures/MyAuthorizationFilter.cs
@@ -0,0 +1,14 @@
+﻿using Hangfire.Dashboard;
+using Microsoft.Owin;
+
+namespace inventoryAppWebUi.Infrastructures
+{
+    public class MyAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            var owinContext = new OwinContext(context.GetOwinEnvironment());
+            return owinContext.Authentication.User.Identity.IsAuthenticated;
+        }
+    }
+}
