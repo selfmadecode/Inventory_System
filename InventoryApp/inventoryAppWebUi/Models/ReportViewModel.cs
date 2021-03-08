7b4788c5b3f30da8fa559649dfe110aa45f1da99
Nwankwu Tochukwu <t.nwankwu@genesystechhub.com>
2021-03-08 12:49:00 +0100

    fixes and updates

---
+++ InventoryApp/inventoryAppWebUi/Models/ReportViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using inventoryAppDomain.Entities;
+using inventoryAppDomain.Entities.Enums;
+
+namespace inventoryAppWebUi.Models
+{
+    public class ReportViewModel
+    {
+        public decimal TotalRevenueForReport { get; set; }
+
+        public List<Drug> ReportDrugs { get; set; }
+        
+        public List<Order> Orders { get; set; }
+    }
+}
