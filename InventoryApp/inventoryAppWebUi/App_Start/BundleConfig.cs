cfcadbc2372ac5d9972c14f49112de87d951ee3a
wisestme01 <c.nwagwu@genesystechhub.com>
2021-03-15 14:12:02 +0100

    add assets

--- InventoryApp/inventoryAppWebUi/App_Start/BundleConfig.cs
+++ InventoryApp/inventoryAppWebUi/App_Start/BundleConfig.cs
@@ -23,11 +23,7 @@ namespace inventoryAppWebUi
                 "~/Content/Assets/js/jquery-3.2.1.min.js",
                 "~/Content/Assets/js/popper.min.js",
                 "~/Content/Assets/js/bootstrap.min.js",
-                "~/Content/Assets/js/custom.js",
-                "~/Content/Assets/js/app.js",
-                "~/Content/Assets/js/chart-custom.js",
-                "~/Content/Assets/js/customizer.js",
-                "~/Content/Assets/js/table-treeview"));
+                "~/Content/Assets/js/custom.js"));
 
 
             bundles.Add(new StyleBundle("~/Content/template/css").Include(
