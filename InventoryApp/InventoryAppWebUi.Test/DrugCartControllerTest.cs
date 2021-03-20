6ca3be0464fba40b44038cee289e97b54c615c44
Ade-Joshe01 <j.adegoke@genesystechhub.com>
2021-03-20 09:48:46 +0100

    Unit Test Feature

---
+++ InventoryApp/InventoryAppWebUi.Test/DrugCartControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+using inventoryAppDomain.Repository;
+using inventoryAppDomain.Services;
+using inventoryAppWebUi.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace InventoryAppWebUi.Test
+{
+    [TestClass]
+    public class DrugCartControllerTest
+    {
+      
+
+     
+
+        [TestMethod]
+        public void IndexTest()
+        {
+           
+
+          Mock<IDrugCartService> _mockDrug = new Mock<IDrugCartService>();
+            //_mockDrug.Setup(z => z.RefreshCart(userId.ToString()));
+           var controller = new DrugCartController(_mockDrug.Object);
+
+            var result = controller.RemoveAllCart() as ViewResult;
+
+            Assert.AreNotEqual("Index", result.ViewName);
+        }
+    }
+}
---
+++ InventoryApp/InventoryAppWebUi.Test/HomeControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Mvc;
+using inventoryAppDomain.Repository;
+using inventoryAppDomain.Services;
+using inventoryAppWebUi.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace InventoryAppWebUi.Test
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        private HomeController _acontroller;
+        private Mock<IDrugCartService> _drugCartService;
+        private Mock<IDrugService> _drugService;
+        private Mock<ISupplierService> _suppService;
+        private Mock<IOrderService> _orderService;
+     
+        public HomeControllerTest()
+        {
+            _drugCartService = new Mock<IDrugCartService>();
+            _drugService = new Mock<IDrugService>();
+            _orderService = new Mock<IOrderService>();
+            _suppService = new Mock<ISupplierService>();
+
+            _acontroller = new HomeController(_suppService.Object, _drugService.Object, _drugCartService.Object, _orderService.Object);
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = _acontroller.Index() as ViewResult;
+
+            Assert.AreEqual("Index", result.ViewName);
+
+        }
+    }
+}
