6c4ec2371caa74646641f4c3cb6cc7b798f28759
Nwankwu Tochukwu <t.nwankwu@genesystechhub.com>
2021-03-22 09:50:03 +0100

    refactor of payment module

--- InventoryApp/inventoryAppDomain/Repository/MonnifyPaymentService.cs
+++ InventoryApp/inventoryAppDomain/Repository/MonnifyPaymentService.cs
@@ -8,7 +8,7 @@ using System.Web;
 using System.Web.Configuration;
 using AutoMapper;
 using inventoryAppDomain.Entities;
-using inventoryAppDomain.Entities.Dtos;
+using inventoryAppDomain.Entities.Enums;
 using inventoryAppDomain.Entities.MonnifyDtos;
 using inventoryAppDomain.IdentityEntities;
 using inventoryAppDomain.Services;
@@ -24,7 +24,10 @@ namespace inventoryAppDomain.Repository
         private readonly ApplicationDbContext _dbContext;
         private string _contractCode = WebConfigurationManager.AppSettings["monnifyContractCode"];
         private string _loginUrl = "https://sandbox.monnify.com/api/v1/auth/login";
-        private string _initTransactionUrl = "https://sandbox.monnify.com/api/v1/merchant/transactions/init-transaction";
+
+        private string _initTransactionUrl =
+            "https://sandbox.monnify.com/api/v1/merchant/transactions/init-transaction";
+
         private string _verifyTransactionUrl = "https://sandbox.monnify.com/api/v2/transactions/";
         private string _secretKey = WebConfigurationManager.AppSettings["monnifySecretKey"];
         private string _apiKey = WebConfigurationManager.AppSettings["monnifyApiKey"];
@@ -35,34 +38,70 @@ namespace inventoryAppDomain.Repository
             _transactionService = transactionService;
             _dbContext = HttpContext.Current.GetOwinContext().Get<ApplicationDbContext>();
         }
-        
+
         private string GenerateTxRef()
         {
             const int txRefLength = 11;
-        
+
             var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToLower();
             var random = new Random();
-        
+
             var txRef = "MC-" + new string(Enumerable.Repeat(chars, txRefLength)
                 .Select(s => s[random.Next(s.Length)]).ToArray());
-        
+
             var txRefInDb = _dbContext.Transactions.SingleOrDefault(t => t.GeneratedReferenceNumber == txRef);
-        
+
             if (txRefInDb != null)
                 GenerateTxRef();
-        
+
             return txRef;
+        }
+
+        private async Task<ResponseDto> GetClientResponse(string url, HttpRequestMethod requestMethod,
+            string authorizationHeader, object payload = null)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", authorizationHeader);
+
+            switch (requestMethod)
+            {
+                case HttpRequestMethod.POST:
+                {
+                    httpClient.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+                    if (payload == null)
+                    {
+                        var response = await httpClient.PostAsync(url, null);
+                        return JsonConvert.DeserializeObject<ResponseDto>(await response.Content.ReadAsStringAsync());
+                    }
+                    else
+                    {
+                        var content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8,
+                            "application/json");
+                        var response = await httpClient.PostAsync(url, content);
+                        return JsonConvert.DeserializeObject<ResponseDto>(await response.Content.ReadAsStringAsync());
+                    }
+                }
+                case HttpRequestMethod.GET:
+                {
+                    var response = await httpClient.GetAsync(url);
+                    return JsonConvert.DeserializeObject<ResponseDto>(await response.Content.ReadAsStringAsync());
+                }
+                default: throw new Exception("Something Went Wrong");
+            }
+        }
+
+        private T GetResponseBody<T>(ResponseDto responseDto)
+        {
+            return JsonConvert.DeserializeObject<T>(responseDto.responseBody.ToString());
         }
 
         private async Task<ResponseDto> Login()
         {
-            var httpClient = new HttpClient {BaseAddress = new Uri(_loginUrl)};
             var encodedString = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{_apiKey}:{_secretKey}"));
-            httpClient.DefaultRequestHeaders.Add("Authorization", $"Basic {encodedString}");
-
-            var response = await httpClient.PostAsync(_loginUrl, null);
-            return JsonConvert.DeserializeObject<ResponseDto>(await response.Content.ReadAsStringAsync());
+            return await GetClientResponse(_loginUrl, HttpRequestMethod.GET, $"Basic {encodedString}");
         }
+
         public async Task<InitTransactionResponseBody> InitiatePayment(int orderId)
         {
             var order = _orderService.GetOrderById(orderId);
@@ -70,45 +109,28 @@ namespace inventoryAppDomain.Repository
             payload.contractCode = _contractCode;
             payload.paymentReference = GenerateTxRef();
             payload.paymentDescription = $"Order Payment for {payload.customerName}";
-            var httpClient = new HttpClient();
             var encodedString = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{_apiKey}:{_secretKey}"));
-            httpClient.DefaultRequestHeaders.Add("Authorization", $"Basic {encodedString}");
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            var content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
-            var response = await httpClient.PostAsync(_initTransactionUrl, content);
-
-            var responseContent = JsonConvert.DeserializeObject<ResponseDto>(await response.Content.ReadAsStringAsync());
-            var responseBody =
-                JsonConvert.DeserializeObject<InitTransactionResponseBody>(responseContent.responseBody.ToString());
-            await _transactionService.CreateTransaction(responseBody.transactionReference, payload.paymentReference, orderId);
+            var response = await GetClientResponse(_initTransactionUrl, HttpRequestMethod.POST,
+                $"Basic {encodedString}", payload);
+            var responseBody = GetResponseBody<InitTransactionResponseBody>(response);
+            await _transactionService.CreateTransaction(responseBody.transactionReference, payload.paymentReference,
+                orderId);
             return responseBody;
         }
 
         public async Task<bool> VerifyPayment(string paymentReference)
         {
-            var accessToken = JsonConvert.DeserializeObject<AccessTokenResponseBody>(JsonConvert.SerializeObject((await Login()).responseBody)).accessToken;
+            var accessToken = JsonConvert
+                .DeserializeObject<AccessTokenResponseBody>((await Login()).responseBody.ToString())
+                .accessToken;
             var transaction = await _transactionService.GetTransactionByGeneratedRef(paymentReference);
             var encodedUrl = HttpUtility.UrlEncode(transaction.ReferenceNumber);
             var fullPath = $"{_verifyTransactionUrl}{encodedUrl}";
-            var httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            var response = await httpClient.GetAsync(fullPath);
-            var responseContent =
-                JsonConvert.DeserializeObject<ResponseDto>(await response.Content.ReadAsStringAsync());
-
-            var responseBody =
-                JsonConvert.DeserializeObject<VerificationResponseBody>(responseContent.responseBody.ToString());
-
-            if (responseBody.paymentStatus.Equals("PAID"))
-            {
-                return true;
-            }
-
+            var response =
+                await GetClientResponse(fullPath, HttpRequestMethod.GET, $"Bearer {accessToken}");
+            var responseBody = GetResponseBody<VerificationResponseBody>(response);
+            if (responseBody.paymentStatus.Equals("PAID")) return true;
             return false;
         }
-
     }
 }
